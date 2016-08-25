b14ac782f819de32a1159aa2dcffb0003c04e9c4
shishang.fxg <shishang.fxg@alibaba-inc.com>
2016-08-25 17:28:03 +0800

    mark

--- src/main/java/com/xiaogang/ExcelUtilsTest.java
+++ src/main/java/com/xiaogang/ExcelUtilsTest.java
@@ -9,13 +9,10 @@ import java.io.FileInputStream;
 public class ExcelUtilsTest {
 
     public static void main(String[] args) throws Exception {
-//        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-//        loggerContext.getLogger(ExcelUtils.class).setLevel(Level.DEBUG);
         ExcelResolveResult<FlexItemEO> result = ExcelUtils.resolveExcel(new FileInputStream("/Users/xiaogangfan/Downloads/item.xlsx"), FlexItemEO.class);
-//        ExcelResolveResult<FlexItemEO> result = ExcelUtils.resolveExcel(new FileInputStream("/Users/xiaogang/Downloads/test.xlsx"), FlexItemEO.class);
-//        ExcelResolveResult<InPriceSheetItemEO> result = ExcelUtils.resolveExcel(new FileInputStream("/Users/xiaogang/Downloads/进价改价excel模版.xlsx"), InPriceSheetItemEO.class);
         System.out.println(result.getData().size());
         System.out.println(result);
+
     }
 
 }
