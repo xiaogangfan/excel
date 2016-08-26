edc5f9440fbd6ec70c8e0aeed3db1247f088b23a
shishang.fxg <shishang.fxg@alibaba-inc.com>
2016-08-26 09:49:08 +0800

    mark

--- src/test/java/com/xiaogang/ExcelUtilsTest.java
+++ src/test/java/com/xiaogang/ExcelUtilsTest.java
@@ -1,17 +1,24 @@
 package com.xiaogang;
 
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
 import java.io.FileInputStream;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.fail;
+
 /**
  * @author xiaogang
  * @date 2016-06-28
  */
 public class ExcelUtilsTest {
 
-    public static void main(String[] args) throws Exception {
+    @Test
+    public void testExcel() throws Exception {
 
         try {
             ExcelResolveResult<FlexItemEO> result = ExcelUtils.resolveExcel(new FileInputStream("/Users/xiaogangfan/Downloads/item.xlsx"), FlexItemEO.class);
+            System.out.println(JSONObject.toJSONString(result));
         } catch (Exception e) {
             e.printStackTrace();
             fail("excel 解析失败："+e.getMessage());
